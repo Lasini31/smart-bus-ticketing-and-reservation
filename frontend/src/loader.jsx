207803a3c9d15d1a2d26912e3ea83a5b50417b7a
Dulith Kavinda <143782811+Dulith-Kavinda@users.noreply.github.com>
2026-05-06 03:00:53 +0530

    Initial Frontend files upload
    Initial Frontend files upload

---
+++ frontend/src/loader.jsx
@@ -0,0 +1,10 @@
+// import { PacmanLoader } from "react-spinners"
+
+// export default function Loader() {
+//     return (
+//         <div className="h-[100%] w-[100%] flex justify-center items-center">
+//             <PacmanLoader color="#0395a1"/>
+//         </div>
+
+//     )
+// }
