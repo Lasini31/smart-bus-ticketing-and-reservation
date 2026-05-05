e5974c71218d72860cc49a1dabb2dba0b772a39c
dilith813 <dilithhemaka@gmail.com>
2026-05-05 18:03:46 +0530

    created Files

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import { Outlet } from 'react-router-dom'
 import Header from './components/header'
+import PaymentPage from '.pages/PaymentPage';
 
 export default function Layout(){
 
