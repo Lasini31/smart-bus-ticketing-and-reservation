0ff09c7ee751de5469cc2a32ca965c347b51380d
Lochana Basnayaka <ranidu.jaya171@gmail.com>
2026-06-17 14:39:13 +0530

    dto user

--- backend/busmanagement/src/main/java/com/STAR/busmanagement/user/dto/UpdateProfileRequest.java
+++ backend/busmanagement/src/main/java/com/STAR/busmanagement/user/dto/UpdateProfileRequest.java
@@ -1,1 +1,13 @@
-user
+package com.STAR.busmanagement.user.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UpdateProfileRequest {
+    private String name;
+    private String contactNumber;
+}
